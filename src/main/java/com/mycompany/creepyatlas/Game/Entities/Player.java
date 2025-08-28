f98fe9201e7a3c55ccba84a130b62e7e85102552
LauraFranco404 <laurafranco1116@gmail.com>
2025-08-28 16:34:35 -0500

    player movement refactor

--- src/main/java/com/mycompany/creepyatlas/Game/Entities/Player.java
+++ src/main/java/com/mycompany/creepyatlas/Game/Entities/Player.java
@@ -1,5 +1,8 @@
 package com.mycompany.creepyatlas.Game.Entities;
 
+import com.mycompany.creepyatlas.Audio.AudioListener3D;
+import com.mycompany.creepyatlas.Enums.Enums.*;
+
 public class Player extends Entity {
     public Player(int x, int y) {
         super(x, y);
@@ -8,5 +11,12 @@ public class Player extends Entity {
     @Override
     public char getSymbol() {
         return 'P';
+    }
+
+    @Override
+    public void move(Direction direction)
+    {
+        super.move(direction);
+        AudioListener3D.setPosition(x, 0, y);
     }
 }
