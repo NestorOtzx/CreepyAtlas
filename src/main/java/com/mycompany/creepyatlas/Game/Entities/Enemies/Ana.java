1f1d2a2326b4e95c8a9500d7e90b188ee0393357
LauraFranco404 <laurafranco1116@gmail.com>
2025-09-08 16:50:04 -0500

    ana bug fixed

--- src/main/java/com/mycompany/creepyatlas/Game/Entities/Enemies/Ana.java
+++ src/main/java/com/mycompany/creepyatlas/Game/Entities/Enemies/Ana.java
@@ -8,7 +8,6 @@ public class Ana extends Enemy {
 
     @Override
     public char getSymbol() {
-<<<<<<< HEAD
         return 'N';
 
     }
@@ -16,12 +15,5 @@ public class Ana extends Enemy {
     @Override
     public String getBaseAudioPath(){
         return "/audios/ana_mono.wav";
-=======
-        if (!is_dead)
-        {
-            return 'N';
-        }
-        return super.getSymbol();
->>>>>>> 072dec4c57f41a085881d05e3a99d84897114337
     }
 }
